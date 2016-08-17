e6b65dd5117baa2a411337e6a030c4ee4b530a2d
yang <edyang91@gmail.com>
2016-08-17 13:03:47 +1000

    Added a comment

--- app/src/main/java/com/example/utshelps/MainActivity.java
+++ app/src/main/java/com/example/utshelps/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    // adding a comment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
