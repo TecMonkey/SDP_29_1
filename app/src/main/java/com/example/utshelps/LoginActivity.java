656658007dfd61b1fdc3fbecd5130d8087a34f56
Yaseen Kadir <yaseen.kadir@student.uts.edu.au>
2016-08-23 01:42:25 +1000

    Validate and display error messages for login details

--- app/src/main/java/com/example/utshelps/LoginActivity.java
+++ app/src/main/java/com/example/utshelps/LoginActivity.java
@@ -4,8 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.regex.Pattern;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText mStudentIdEditText;
+    private EditText mPasswordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,13 +22,47 @@ public class LoginActivity extends AppCompatActivity {
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                signIn();
+                attemptSignIn();
             }
         });
+
+        mStudentIdEditText = (EditText) findViewById(R.id.activity_login_student_id_edit_text);
+        mPasswordEditText = (EditText) findViewById(R.id.activity_login_student_password_edit_text);
     }
 
-    public void signIn() {
-        setResult(Constants.LOGIN_ACTIVITY_SUCCESS);
-        finish();
+    public void attemptSignIn() {
+        boolean error = false;
+
+        String studentId = mStudentIdEditText.getText().toString();
+        if (!isStudentIdValid(studentId)) {
+            mStudentIdEditText.setError(getString(R.string.error_student_id_numbers));
+            mStudentIdEditText.requestFocus();
+            error = true;
+        }
+
+        String password = mPasswordEditText.getText().toString();
+        if (password.length() < 6) {
+            mPasswordEditText.setError(getString(R.string.error_password_six_characters));
+            mPasswordEditText.requestFocus();
+            error = true;
+        } else if (password.length() > 20) {
+            mPasswordEditText.setError(getString(R.string.error_password_twenty_characters));
+            mPasswordEditText.requestFocus();
+            error = true;
+        }
+
+        if (!error) {
+            setResult(Constants.LOGIN_ACTIVITY_SUCCESS);
+            finish();
+        }
+    }
+
+    private boolean isStudentIdValid(String studentId) {
+        Pattern studentIdPattern = Pattern.compile("^[0-9]{8}$");
+        return isPatternValid(studentIdPattern, studentId);
+    }
+
+    private boolean isPatternValid(Pattern regexPattern, String text) {
+        return regexPattern.matcher(text).matches();
     }
 }
