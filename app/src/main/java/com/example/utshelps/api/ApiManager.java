6093a820df4415541fdb6f6d9ceca0fcfb016ed3
Yaseen Kadir <yaseen.kadir@student.uts.edu.au>
2016-10-04 00:51:12 +1100

    Added LoginRequest model and defined interface methods to login.

--- app/src/main/java/com/example/utshelps/api/ApiManager.java
+++ app/src/main/java/com/example/utshelps/api/ApiManager.java
@@ -1,5 +1,7 @@
 package com.example.utshelps.api;
 
+import com.example.utshelps.api.model.BaseResponse;
+import com.example.utshelps.api.model.LoginRequest;
 import com.example.utshelps.api.model.WorkshopSetResponse;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -19,18 +21,10 @@ import retrofit2.converter.gson.GsonConverterFactory;
  * Created by Yaseen on 29/08/2016.
  */
 public class ApiManager {
-    private static ApiManager mApiManager;
-    private UtsHelpsService mService;
-
     private static final String BASE_URL = "http://188.166.249.70/api/";
     private static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ssZ";
-
-    public static ApiManager getInstance() {
-        if (mApiManager == null) {
-            mApiManager = new ApiManager();
-        }
-        return mApiManager;
-    }
+    private static ApiManager mApiManager;
+    private UtsHelpsService mService;
 
     private ApiManager() {
         // The interceptor is used to view the requests and responses sent over the wire.
@@ -53,6 +47,13 @@ public class ApiManager {
         mService = retrofit.create(UtsHelpsService.class);
     }
 
+    public static ApiManager getInstance() {
+        if (mApiManager == null) {
+            mApiManager = new ApiManager();
+        }
+        return mApiManager;
+    }
+
     /**
      * Makes a call to the server to retrieve a list of workshops.
      *
@@ -61,5 +62,15 @@ public class ApiManager {
     public void getWorkshopSetList(Callback callback) {
         Call<WorkshopSetResponse> workshopsCall = mService.getWorkshopSetList();
         workshopsCall.enqueue(callback);
+    }
+
+    /**
+     * Logs the user in to the system.
+     *
+     * @param callback
+     */
+    public void login(Callback callback, LoginRequest loginRequest) {
+        Call<BaseResponse> loginCall = mService.login(loginRequest);
+        loginCall.enqueue(callback);
     }
 }
--- app/src/main/java/com/example/utshelps/api/UtsHelpsService.java
+++ app/src/main/java/com/example/utshelps/api/UtsHelpsService.java
@@ -1,9 +1,13 @@
 package com.example.utshelps.api;
 
+import com.example.utshelps.api.model.BaseResponse;
+import com.example.utshelps.api.model.LoginRequest;
 import com.example.utshelps.api.model.WorkshopSetResponse;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 /**
  * Interface that is used by Retrofit to make network calls.
@@ -13,4 +17,7 @@ import retrofit2.http.GET;
 public interface UtsHelpsService {
     @GET("workshop/workshopSets/")
     Call<WorkshopSetResponse> getWorkshopSetList();
+
+    @POST("student/login/")
+    Call<BaseResponse> login(@Body LoginRequest loginRequest);
 }
