dff88378a0231ad3daf0bf2192dc3332b0b9b8c5
Yaseen Kadir <yaseen.kadir@student.uts.edu.au>
2016-10-03 12:29:43 +1100

    Added constant class for all JsonKeys

---
+++ app/src/main/java/com/example/utshelps/util/JsonKeys.java
@@ -0,0 +1,78 @@
+package com.example.utshelps.util;
+
+/**
+ * Created by yaseen on 03/10/16
+ */
+public class JsonKeys {
+    // Response keys
+    public static final String IS_SUCCESS = "IsSuccess";
+    public static final String DISPLAY_MESSAGE = "DisplayMessage";
+    public static final String RESULTS = "Results";
+
+    // General Keys
+    public static final String CAMPUS = "Campus";
+    public static final String NAME = "Name";
+    public static final String ARCHIVED = "Archived";
+    public static final String ARCHIVED_DATE = "ArchivedDate";
+
+    // Student Model Keys
+    public static final String STUDENT_ID = "StudentId";
+    public static final String DOB = "DateOfBirth";
+    public static final String FIRST_NAME = "FirstName";
+    public static final String LAST_NAME = "LastName";
+    public static final String PREFERRED_NAME = "PreferredName";
+    public static final String CONTACT_NUMBER = "ContactNumber";
+    public static final String GENDER = "Gender";
+    public static final String FIRST_LANGUAGE = "FirstLanguage";
+    public static final String COUNTRY_OF_ORIGIN = "CountryOrigin";
+    public static final String BACKGROUND = "Background";
+
+    public static final String DEGREE_TYPE = "Degree";
+    public static final String DEGREE_NAME = "DegreeName";
+    public static final String DEGREE_CODE = "DegreeCode";
+    public static final String DEGREE_STATUS = "Status";
+    public static final String DEGREE_YEARS = "DegreeDetails";
+
+    public static final String HSC = "HSC";
+    public static final String HSC_MARK = "HSCMark";
+    public static final String IELTS = "IELTS";
+    public static final String IELTS_MARK = "IELTSMark";
+    public static final String TOEFL = "TOEFL";
+    public static final String TOEFL_MARK = "TOEFLMark";
+    public static final String TAFE = "TAFE";
+    public static final String TAFE_MARK = "TAFEMark";
+    public static final String CULT = "CULT";
+    public static final String CULT_MARK = "CULTMark";
+    public static final String INSEARCH_DEEP = "InsearchDEEP";
+    public static final String INSEARCH_DEEP_MARK = "InsearchDEEPMark";
+    public static final String INSEARCH_DIPLOMA = "InsearchDiploma";
+    public static final String INSEARCH_DIPLOMA_MARK = "InsearchDiplomaMark";
+    public static final String FOUNDATION_COURSE = "FoundationCourse";
+    public static final String FOUNDATION_COURSE_MARK = "FoundationCourseMark";
+
+    public static final String PASSWORD = "Password";
+
+
+    // Workshop Model keys
+    public static final String WORKSHOP_ID = "WorkshopId";
+    public static final String TOPIC = "Topic";
+    public static final String DESCRIPTION = "Description";
+    public static final String TARGETING_GROUP = "TargetingGroup";
+    public static final String CUTOFF = "Cutoff";
+    public static final String TYPE = "Type";
+    public static final String REMINDER_NUM = "ReminderNum";
+    public static final String REMINDER_SENT = "ReminderSent";
+    public static final String WORKSHOP_SESSIONS = "WorkshopSessions";
+
+
+    // WorkshopSession Model keys
+    public static final String WORKSHOP_SESSION_ID = "WorkshopSessionId";
+    public static final String START_DATE = "StartDate";
+    public static final String END_DATE = "EndDate";
+    public static final String MAXIMUM = "Maximum";
+    public static final String PLACES_AVAILABLE = "PlacesAvailable";
+
+
+    // WorkshopSet Model Keys
+    public static final String WORKSHOP_SET_ID = "WorkshopSetId";
+}
