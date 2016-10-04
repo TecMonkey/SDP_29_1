1161f1c13dcc73fc444f3722475954fc13646713
FERDY-PC\Ferdy <ferdy.anata-1@student.uts.edu.au>
2016-10-05 10:54:22 +1100

    no message

--- app/src/main/java/com/example/utshelps/activity/SessionActivity.java
+++ app/src/main/java/com/example/utshelps/activity/SessionActivity.java
@@ -1,11 +1,11 @@
 package com.example.utshelps.activity;
-
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
-
+import android.widget.AdapterView;
+import android.widget.ListView;
 import com.example.utshelps.R;
 
 /**Activity that shows sessions list of a workshop
@@ -13,9 +13,19 @@ import com.example.utshelps.R;
  */
 
 public class SessionActivity extends AppCompatActivity {
+    private ListView mListView;
+    private String[] mSession_names;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_session);
+        mListView = (ListView)findViewById(R.id.list_view); //Create reference to listView in activity_session
+        mSession_names = getResources().getStringArray(R.array.session_names); //Grabs the string arrays in string.xml
+        int i = 0;
+        for(String titles: mSession_names){
+            SessionsData sessionData = new SessionsData(titles, mSession_names[i]);
+        }
     }
 
     @Override
@@ -24,10 +34,6 @@ public class SessionActivity extends AppCompatActivity {
         return true;
     }
 
-    public void sessionDetails() {
-
-
-    }
 
 
 
--- app/src/main/java/com/example/utshelps/activity/SessionsData.java
+++ app/src/main/java/com/example/utshelps/activity/SessionsData.java
@@ -5,4 +5,20 @@ package com.example.utshelps.activity;
  */
 
 public class SessionsData {
+    private String mSession_names;
+
+    public SessionsData(String mSession_names) {
+
+    }
+
+
+
+    public String getmSession_names() {
+        return mSession_names;
+    }
+
+    public void setmSession_names(String mSession_names) {
+        this.mSession_names = mSession_names;
+    }
 }
+
