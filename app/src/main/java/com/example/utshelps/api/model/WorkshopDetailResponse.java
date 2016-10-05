47f5b80df3ead7af0636ac7f74c9d85b120acff6
FERDY-PC\Ferdy <ferdy.anata-1@student.uts.edu.au>
2016-10-06 00:37:38 +1100

    Created WorkshopDetailResponse model in the api and WorkshopDetail activity in model.

--- app/src/main/java/com/example/utshelps/api/model/WorkshopDetailResponse.java
+++ app/src/main/java/com/example/utshelps/api/model/WorkshopDetailResponse.java
@@ -1,8 +1,24 @@
 package com.example.utshelps.api.model;
 
+import com.example.utshelps.model.WorkshopDetail;
+import com.example.utshelps.util.JsonKeys;
+import com.google.gson.JsonObject;
+import com.google.gson.annotations.SerializedName;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 /**
  * Created by Ferdy on 10/5/2016.
  */
 
-public class WorkshopDetailResponse {
+public class WorkshopDetailResponse extends BaseResponse {
+
+    //JsonKeys.RESULTS are in ArrayList
+    @SerializedName(JsonKeys.RESULTS)
+    private ArrayList <WorkshopDetail> mWorkshopDetails;
+
+    public ArrayList <WorkshopDetail> getWorkshopDetailArrayList()
+    { return mWorkshopDetails; }
+
 }
